c992d97dda51e0b4b66c20fa5f94f96559527c92
Singularity-one <52284919+Singularity-one@users.noreply.github.com>
2025-01-11 14:49:23 +0800

    try

--- src/route/route.js
+++ src/route/route.js
@@ -1,13 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../views/HomePage.vue';
-import AboutPage from '../views/AboutPage.vue';
 import DayEn15 from '../views/DayEn15.vue';
 import dayOne from '../views/dayOne.vue';
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/Home', name: 'Home', component: HomePage },
-  { path: '/about', name: 'About', component: AboutPage },
   { path: '/DayEn15', name: 'DayEn15', component: DayEn15 },
   { path: '/dayOne', name: 'dayOne', component: dayOne },
 ];
